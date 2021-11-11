77ea9239f960cf2cd6d4f74785dfefc5009ee575
devOOs   maaroufi hicham <ishamaaroufi@gmail.com>
2021-11-11 13:41:54 +0100

    Update napalmCisco.py

--- napalmCisco.py
+++ napalmCisco.py
@@ -1,6 +1,7 @@
 from napalm1 import get_network_driver
 import json
 
+
 #list ip
 listip = [
     '1.1.1.1',
@@ -9,10 +10,13 @@ listip = [
 
 
 for ipAddress in listip:
+    
     print('list ip is'+str(ipaddress))
     devices = get_network_driver('ios')
     conects = devices(ipAddress, 'admin', 'cisco')
     conects.open()
+    
     output = conects.get_interfaces()
     print(json.dumps(output, sort_keys=True, indent=4))
+   
     output.close()
